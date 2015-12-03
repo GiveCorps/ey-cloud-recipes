89bd9aa733f5ba68051203a760311512402d754c
Timothy King <tmk@lordzork.com>
2015-12-03 17:25:57 -0500

    Attempt to get app_name in custom crontab recipe

--- cookbooks/crontab_custom/recipes/default.rb
+++ cookbooks/crontab_custom/recipes/default.rb
@@ -5,14 +5,15 @@
 
 case node[:instance_role]
 when 'app', 'app_master', 'solo'
-  %w(rake_cleanup_tmp.sh rake_cleanup_sessions.sh).each do |shell_script|
-    template "/etc/cron.weekly/#{shell_script}" do
-      mode 0755
-      source "#{shell_script}.erb"
-      variables({
-        app_dir: "/data/#{node[:app_name]}/current"
-      })
+  node[:applications].each do |app_name, data|
+    %w(rake_cleanup_tmp.sh rake_cleanup_sessions.sh).each do |shell_script|
+      template "/etc/cron.weekly/#{shell_script}" do
+        mode 0755
+        source "#{shell_script}.erb"
+        variables({
+          app_dir: "/data/#{app_name}/current"
+        })
+      end
     end
   end
-
 end
