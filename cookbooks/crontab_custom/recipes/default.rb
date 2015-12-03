8aa076bd56b41d490f948494474c69123079c6b2
Timothy King <tmk@lordzork.com>
2015-12-03 16:57:39 -0500

    Corrects syntax error

--- cookbooks/crontab_custom/recipes/default.rb
+++ cookbooks/crontab_custom/recipes/default.rb
@@ -6,13 +6,15 @@
 case node[:instance_role]
 when 'app', 'app_master', 'solo'
   app_dir = "/data/#{app_name}/current"
+
   %w(rake_cleanup_tmp.sh rake_cleanup_sessions.sh).each do |shell_script|
-  template "/etc/cron.weekly/#{shell_script}" do
-    mode 755
-    source "#{shell_script}.erb"
-    variables({
-      app_dir: app_dir
-    })
+    template "/etc/cron.weekly/#{shell_script}" do
+      mode 755
+      source "#{shell_script}.erb"
+      variables({
+        app_dir: app_dir
+      })
+    end
   end
 
 end
