60e6c39678635b7e6b9845c5baf5732f4a989c5c
Timothy King <tmk@lordzork.com>
2015-12-03 17:14:46 -0500

    Fixes undefined variable

--- cookbooks/crontab_custom/recipes/default.rb
+++ cookbooks/crontab_custom/recipes/default.rb
@@ -5,14 +5,12 @@
 
 case node[:instance_role]
 when 'app', 'app_master', 'solo'
-  app_dir = "/data/#{node[:app_name]}/current"
-
   %w(rake_cleanup_tmp.sh rake_cleanup_sessions.sh).each do |shell_script|
     template "/etc/cron.weekly/#{shell_script}" do
       mode 755
       source "#{shell_script}.erb"
       variables({
-        app_dir: app_dir
+        app_dir: "/data/#{node[:app_name]}/current"
       })
     end
   end
