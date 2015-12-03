1deacde887d1c62c01fa8e51ec070cfca43c853c
Timothy King <tmk@lordzork.com>
2015-12-03 17:20:38 -0500

    Attempt to fix mode on cron files

--- cookbooks/crontab_custom/recipes/default.rb
+++ cookbooks/crontab_custom/recipes/default.rb
@@ -7,7 +7,7 @@ case node[:instance_role]
 when 'app', 'app_master', 'solo'
   %w(rake_cleanup_tmp.sh rake_cleanup_sessions.sh).each do |shell_script|
     template "/etc/cron.weekly/#{shell_script}" do
-      mode 755
+      mode 0755
       source "#{shell_script}.erb"
       variables({
         app_dir: "/data/#{node[:app_name]}/current"
