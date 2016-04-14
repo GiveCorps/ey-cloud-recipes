8695667f20be8af66063316cb731a3c4a91383be
Timothy King <tmk@lordzork.com>
2016-04-14 14:44:05 -0400

    Moves tmp cleanup cron script to cron.daily

--- cookbooks/crontab_custom/recipes/default.rb
+++ cookbooks/crontab_custom/recipes/default.rb
@@ -6,7 +6,18 @@
 case node[:instance_role]
 when 'app', 'app_master', 'solo'
   node[:applications].each do |app_name, data|
-    %w(rake_cleanup_tmp.sh rake_cleanup_sessions.sh).each do |shell_script|
+    %w(rake_cleanup_tmp.sh).each do |shell_script|
+      template "/etc/cron.daily/#{shell_script}" do
+        mode 0755
+        source "#{shell_script}.erb"
+        variables({
+          app_dir: "/data/#{app_name}/current",
+          environment: node[:environment][:framework_env]
+        })
+      end
+    end
+
+    %w(rake_cleanup_sessions.sh).each do |shell_script|
       template "/etc/cron.weekly/#{shell_script}" do
         mode 0755
         source "#{shell_script}.erb"
