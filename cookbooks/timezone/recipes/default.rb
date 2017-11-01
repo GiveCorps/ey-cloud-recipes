bb7259954fbf24328ac95323422b4d055f85c1ec
Timothy King <tmk@lordzork.com>
2017-11-01 10:39:55 -0400

    Changes timezone to US/Eastern

--- cookbooks/timezone/recipes/default.rb
+++ cookbooks/timezone/recipes/default.rb
@@ -1,7 +1,7 @@
 # Add your required timezone name here:
 # If using JRuby with Trinidad, you must also modify the env.custom file with the desired timezone
 # ex: add_java_option -Duser.timezone=UTC
-timezone = "EST"
+timezone = "US/Eastern"
 
 unless ['db_master', 'db_slave', 'solo'].include?(node[:instance_role])
   service "vixie-cron"
